ba8e65c2ef0a0ca50f3ad7a98b30ac8ea02db180
IMHOEDE AMOS OSIKEMHEKHA <osiamijnr@gmai.com>
2026-02-17 04:37:27 +0100

    start up file

---
+++ src/Components/Header.tsx
@@ -0,0 +1,23 @@
+
+const Header = ()=>{ 
+  <div className="">  
+  <nav> 
+    <h1>FocusOS</h1>
+    <ul>  
+      <li><a href="">Dashboard</a></li>
+      <li><a href="">Analytics</a></li>
+      <li><a href="">Planner</a></li>
+      <li><a href="">Reflection</a></li>
+    </ul> 
+    <ul>
+      <li><a href="">Logout</a></li> 
+      <button>Settings</button>
+    </ul>
+  </nav>
+
+
+  </div>
+}
+
+
+export default Header
---
+++ src/Pages/Dashboard.tsx
@@ -0,0 +1,12 @@
+import Header from "../Components/Header"; 
+
+const Dashboard = () => {
+  // Component logic and state hooks go here
+  return (
+    <div>
+      <Header></Header>
+    </div>
+  );
+};
+
+export default Dashboard;
