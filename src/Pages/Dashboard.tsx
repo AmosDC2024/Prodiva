7a035773f61916abede55261a0d787568d8de109
IMHOEDE AMOS OSIKEMHEKHA <osiamijnr@gmai.com>
2026-03-07 02:19:12 +0100

    structure dashboard.tsx to handle layout logic from other components

--- src/Pages/Dashboard.tsx
+++ src/Pages/Dashboard.tsx
@@ -1,10 +1,18 @@
-import Header from "../Components/Header"; 
+import Header from "../Components/Header";
+import SearchBar from "../Components/SearchBar";
+import ActiveTask from "../Components/ActiveTask";
 
 const Dashboard = () => {
   // Component logic and state hooks go here
   return (
-    <div>
-      <Header></Header>
+    <div className="flex max-w-[1200px] mx-auto">
+      <div className="w-full min-h-screen bg-[#0B1120] md:flex">
+        <Header></Header>
+        <div className="w-full">
+          <SearchBar />
+          <ActiveTask />
+        </div>
+      </div>
     </div>
   );
 };
